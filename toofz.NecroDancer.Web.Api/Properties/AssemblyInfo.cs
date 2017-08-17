330ae3608a8951fcb5cbf0527a974b15fd0cd48a
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-17 15:02:57 -0400

    v3.1.1

--- toofz.NecroDancer.Web.Api/Properties/AssemblyInfo.cs
+++ toofz.NecroDancer.Web.Api/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("toofz NecroDancer")]
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2015")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.1.1.0")]
 
 [assembly: InternalsVisibleTo("toofz.NecroDancer.Web.Api.Tests")]
