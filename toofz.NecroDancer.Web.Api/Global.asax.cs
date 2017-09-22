1511298d54b8656c7dc591704d9f3e5c57cd6f38
Leonard Thieu <leonard.thieu@gmail.com>
2017-09-22 13:41:36 -0400

    Remove unused using.

--- toofz.NecroDancer.Web.Api/Global.asax.cs
+++ toofz.NecroDancer.Web.Api/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
