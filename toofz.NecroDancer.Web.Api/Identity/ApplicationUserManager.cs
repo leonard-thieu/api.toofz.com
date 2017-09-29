960c26708184b18ce1def19bbbdc0f1b13c679b1
Leonard Thieu <leonard.thieu@gmail.com>
2017-09-29 13:51:22 -0400

    Add tests.

--- toofz.NecroDancer.Web.Api/Identity/ApplicationUserManager.cs
+++ toofz.NecroDancer.Web.Api/Identity/ApplicationUserManager.cs
@@ -6,7 +6,7 @@ using Microsoft.Owin;
 namespace toofz.NecroDancer.Web.Api.Identity
 {
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
-    public sealed class ApplicationUserManager : UserManager<ApplicationUser>
+    public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
