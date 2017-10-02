06137e32fa811c49b37bd593e005bfa9b4ea8154
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-02 10:41:28 -0400

    Add redirect to documentation at root URL.

---
+++ toofz.NecroDancer.Web.Api.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using toofz.NecroDancer.Web.Api.Controllers;
+
+namespace toofz.NecroDancer.Web.Api.Tests.Controllers
+{
+    class HomeControllerTests
+    {
+        [TestClass]
+        public class Constructor
+        {
+            [TestMethod]
+            public void ReturnsInstance()
+            {
+                // Arrange -> Act
+                var controller = new HomeController();
+
+                // Assert
+                Assert.IsInstanceOfType(controller, typeof(HomeController));
+            }
+        }
+
+        [TestClass]
+        public class IndexMethod
+        {
+            [TestMethod]
+            public void ReturnsRedirect()
+            {
+                // Arrange
+                var controller = new HomeController();
+
+                // Act
+                var result = controller.Index();
+
+                // Assert
+                Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+            }
+        }
+    }
+}
---
+++ toofz.NecroDancer.Web.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using toofz.NecroDancer.Web.Api.Areas.HelpPage.Controllers;
+
+namespace toofz.NecroDancer.Web.Api.Controllers
+{
+    public sealed class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction(nameof(HelpController.Index), "Help");
+        }
+    }
+}
