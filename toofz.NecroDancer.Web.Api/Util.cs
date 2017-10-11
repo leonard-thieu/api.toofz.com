e08120d05c6962a1b302ba76051f95fb7e084e8c
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-11 15:55:17 -0400

    Update packages.

---
+++ toofz.NecroDancer.Web.Api/Util.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace toofz.NecroDancer.Web.Api
+{
+    static class Util
+    {
+        public static string GetEnvVar(string variable)
+        {
+            return Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.Machine) ??
+                throw new ArgumentNullException(null, $"The environment variable '{variable}' must be set.");
+        }
+    }
+}
