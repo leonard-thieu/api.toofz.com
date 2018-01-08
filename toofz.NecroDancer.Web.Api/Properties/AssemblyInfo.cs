81881c3347ccaaa1b0585bc6f1f4dfb6bcee95a5
Leonard Thieu <leonard.thieu@gmail.com>
2018-01-08 12:24:18 -0500

    fix(build): Use toofz Build.

--- toofz.NecroDancer.Web.Api/Properties/AssemblyInfo.cs
+++ toofz.NecroDancer.Web.Api/Properties/AssemblyInfo.cs
@@ -2,7 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.5.12.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-dev")]
 
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
 [assembly: AssemblyProduct("toofz")]
